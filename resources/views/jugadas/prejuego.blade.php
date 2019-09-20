144b036a19c0d63b959cc90d1050ca4854e58ad1
Dario Marañes <dario.sjs@gmail.com>
2019-09-20 19:41:39 -0300

    Cambio estilo a la vista de prejuego

--- resources/views/jugadas/prejuego.blade.php
+++ resources/views/jugadas/prejuego.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <div class="container">
         <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 class="display-4">No te detengas, seguí jugando!</h1>
+                <p class="lead">Dale 'click' nuevamente al botón y seguí disfrutando de Embirrados.</p>
                 <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                <a class="btn btn-primary btn-lg" href="/juego" role="button">¡Jugar ahora!</a>
+                
+                <p class="lead text-center">
+                <a class="btn btn-warning btn-lg align-items-center" href="/juego" role="button">¡Continuar jugando!</a>
                 </p>
               </div>
 </div>

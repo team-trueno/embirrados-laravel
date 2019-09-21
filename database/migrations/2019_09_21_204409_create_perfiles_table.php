d779445630e32ee269155b296cf4c5f1c5c09ace
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-21 20:09:36 -0300

    Creo migración de perfil usuario

---
+++ database/migrations/2019_09_21_204409_create_perfiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('owner_id');
+            $table->enum('profile_type', ['jugador', 'admin', 'superadmin'])->default('jugador');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfiles');
+    }
+}

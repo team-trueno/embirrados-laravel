67f40df073f4d517ae7a2edc19d62d03eac397cf
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-21 20:08:36 -0300

    Agregué lógica de busqueda en casacada
    Permite que sólo muestre los permiles y puntajes de aquellos usuarios que son jugadores, no para los que son admin.

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Jugador;
+use Illuminate\Database\Eloquent\Builder;
 
 class RankingController extends Controller
 {
     public function index()
     {
-        $jugadores = Jugador::orderBy('puntos', 'DESC')->paginate(15);
+        $jugadores = Jugador::whereHas('user.perfil', function (Builder $query) {
+            $query->where('profile_type', 'jugador');
+        })->orderBy('puntos', 'DESC')->get();
+        // $jugadores = Jugador::orderBy('puntos', 'DESC')->paginate(15);
         // $usuarios = User::paginate(15);
         return view('ranking', compact('jugadores'));
     }

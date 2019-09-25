5aa22cd960d6c6733c4ac63d51c7e20c10949229
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-09-24 22:16:40 -0300

    Actualizo vista index de Usuarios
    Se deja la vista Usarios@index, para que se oriente a la administración de usuarios.

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -8,58 +8,98 @@
 
                 <div class="card-header d-flex align-items-center justify-content-between">
                     <h4 class="float-left mb-0">Listado de usuarios</h4>
-                    <a href="{{ route('preguntas.create') }}" class="btn btn-warning align-self-center disabled">Añadir Usuario</a>
+                    <a href="{{ route('usuarios.create') }}" class="btn btn-warning align-self-center">Añadir
+                        Usuario</a>
                 </div>
 
                 <div class="card-body">
-                <div class="table-responsive">
-                    <table id="mytable" class="table table-hover">
-                        <thead class="thead-dark">
-                            <th>Avatar</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th class="d-none d-md-table-cell">Email</th>
-                            <th class="d-none d-md-table-cell">Usuario</th>
-                            <th class="d-none d-lg-table-cell">Fecha Nacimiento</th>
-                            <th class="d-none d-lg-table-cell">País</th>
-                            <th class="text-center t-min">Acciones</th>
-                        </thead>
+                    <div class="table-responsive">
+                        <table id="mytable" class="table table-hover">
+                            <thead class="thead-dark">
+                                <th class="t-min">Avatar</th>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th class="d-none d-xl-table-cell">Email</th>
+                                <th class="d-none d-md-table-cell">Usuario</th>
+                                <th class="t-min">Perfil</th>
+                                {{-- <th class="d-none d-lg-table-cell">Fecha Nacimiento</th> --}}
+                                {{-- <th class="d-none d-lg-table-cell">País</th> --}}
+                                <th class="text-center t-min">Acciones</th>
+                            </thead>
 
-                        <tbody>
-                            @forelse ($usuarios as $usuario)
-                            <tr>
-                                <td class="align-middle">
-                                    <img src="{{ asset('storage/img/avatars/'.$usuario->avatar) }}" alt="">
-                                </td>
-                                <td class="align-middle">{{ $usuario->name }}</td>
-                                <td class="align-middle">{{ $usuario->apellido }}</td>
-                                <td class="align-middle d-none d-md-table-cell">{{ $usuario->email }}</td>
-                                <td class="align-middle d-none d-md-table-cell">{{ $usuario->usuario }}</td>
-                                <td class="align-middle d-none d-lg-table-cell">{{ $usuario->fecha_nac }}</td>
-                                <td class="align-middle d-none d-lg-table-cell">{{ $usuario->pais }}</td>
-                                <td  class="align-middle d-flex justify-content-between">
-                                            {{-- <div class="float-right"> --}}
-                                    <a class="btn btn-warning btn-sm" href="{{ route('usuarios.show', $usuario->id) }}"><i class="fas fa-eye d-lg-none"></i><span class="d-none d-lg-block">Detalle</span></a>
+                            <tbody>
+                                @forelse ($usuarios as $usuario)
+                                <tr>
+                                    <td class="align-middle">
+                                        <img src="{{ asset('storage/img/avatars/'.$usuario->avatar) }}" alt="">
+                                    </td>
+                                    <td class="align-middle">{{ $usuario->name }}</td>
+                                    <td class="align-middle">{{ $usuario->apellido }}</td>
+                                    <td class="align-middle d-none d-xl-table-cell">{{ $usuario->email }}</td>
+                                    <td class="align-middle d-none d-md-table-cell">{{ $usuario->usuario }}</td>
+                                    {{-- <td class="align-middle"><span class="btn btn-outline-secondary btn-sm text-capitalize">{{ $usuario->perfil->profile_type }}</span>
+                                    </td> --}}
+                                    <td class="align-middle">
+                                        {{-- <span class="btn btn-outline-secondary btn-sm text-capitalize">{{ $usuario->perfil->profile_type }}</span>
+                                        --}}
+                                        @switch($usuario->perfil->profile_type)
+                                            @case('superadmin')
+                                                <button type="button" class="btn btn-success btn-sm btn-block">
+                                                    <i class="fas fa-crown d-lg-none"></i>
+                                                    <span class="text-capitalize d-none d-lg-block">{{ $usuario->perfil->profile_type }}</span>
+                                                </button>
+
+                                                @break
+                                            @case('admin')
+                                                <button type="button" class="btn btn-outline-danger btn-sm btn-block">
+                                                    <i class="fas fa-user-cog d-lg-none"></i>
+                                                    <span class="text-capitalize d-none d-lg-block">{{ $usuario->perfil->profile_type }}</span>
+                                                </button>
+
+                                                @break
+                                            @default
+                                                <button type="button" class="btn btn-outline-secondary btn-sm btn-block">
+                                                    <i class="fas fa-gamepad d-lg-none"></i>
+                                                    <span class="text-capitalize d-none d-lg-block">{{ $usuario->perfil->profile_type }}</span>
+                                                </button>
+
+                                        @endswitch
+                                    </td>
+                                    {{-- <td class="align-middle d-none d-lg-table-cell">{{ $usuario->fecha_nac }}</td>
+                                    --}}
+                                    {{-- <td class="align-middle d-none d-lg-table-cell">{{ $usuario->pais }}</td> --}}
+                                    <td class="align-middle">
+                                        <div class="d-flex justify-content-between">
+                                            <a class="btn btn-warning btn-sm"
+                                                href="{{ route('usuarios.show', $usuario->id) }}"><i
+                                                    class="fas fa-eye d-lg-none"></i><span
+                                                    class="d-none d-lg-block">Detalle</span></a>
 
 
-                                    <a class="btn btn-outline-warning btn-sm mr-1 ml-1" href="{{ route('usuarios.edit', $usuario->id) }}"><i class="fas fa-edit d-lg-none"></i><span class="d-none d-lg-block">Editar</span></a>
+                                            <a class="btn btn-outline-warning btn-sm mr-1 ml-1"
+                                                href="{{ route('usuarios.edit', $usuario->id) }}"><i
+                                                    class="fas fa-edit d-lg-none"></i><span
+                                                    class="d-none d-lg-block">Editar</span></a>
 
 
-                                   <form class="d-inline" action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
+                                            <form class="d-inline"
+                                                action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
 
                                                 {{-- Aca hay que meter un Modal/Alert que pida confirmacion antes de enviar --}}
-                                        <button class="btn btn-dark btn-sm" type="submit"><i class="fas fa-trash-alt d-lg-none"></i><span class="d-none d-lg-block">Eliminar</span></button>
-                                    </form>
-                                        {{-- </div> --}}
-                                </td>
-                            </tr>
-                            @empty
+                                                <button class="btn btn-dark btn-sm" type="submit"><i
+                                                        class="fas fa-trash-alt d-lg-none"></i><span
+                                                        class="d-none d-lg-block">Eliminar</span></button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @empty
                                 <tr>
                                     <td colspan="12">No hay registros cargados.</td>
                                 </tr>
-                            @endforelse
+                                @endforelse
                             </tbody>
                         </table>
                         {{ $usuarios->links() }}
